af967870bc5c936c6b316a45537d6e286bc332b4
Vlad Korochanskiy <mginster234942@gmail.com>
2022-10-17 20:08:18 +0300

    add default image when not load image

--- client/src/components/UserItem/index.js
+++ client/src/components/UserItem/index.js
@@ -6,6 +6,7 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import defaultImage from "../../assets/images/default.jpg";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import styles from "../UsersList/UserList.module.scss";
+import stringToColour from "../../utils/stringToColour";
 import CONSTANTS from "../../constants";
 const { IMAGE_URL } = CONSTANTS;
 
@@ -13,14 +14,27 @@ const UserItem = (props) => {
   const { user } = props;
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const handlerErrorAvatar = ({ target }) => {
+    target.remove();
+  };
+
   return (
     <article key={user.id} className={styles.card}>
       {user.avatar ? (
-        <img
-          src={`${IMAGE_URL}/${user.avatar}`}
-          className={styles.avatar}
-          alt={user.id}
-        />
+        <div className={styles.card_avatar_wrapper}>
+          <div
+            className={styles.default_image}
+            style={{ backgroundColor: `${stringToColour(user.name)}` }}
+          >
+            {user.name[0]}
+          </div>
+          <img
+            src={`${IMAGE_URL}/${user.avatar}`}
+            className={styles.avatar}
+            alt={user.id}
+            onError={handlerErrorAvatar}
+          />
+        </div>
       ) : (
         <img src={defaultImage} className={styles.avatar} alt={user.name} />
       )}
--- client/src/components/UserProfile/UserProfileCard/index.js
+++ client/src/components/UserProfile/UserProfileCard/index.js
@@ -7,6 +7,7 @@ import UserFormEdit from "../../forms/UserFormEdit";
 
 import styles from "./UserProfileCard.module.scss";
 import CONSTANTS from "../../../constants";
+import stringToColour from "../../../utils/stringToColour";
 const { IMAGE_URL } = CONSTANTS;
 
 const UserProfileCard = ({
@@ -20,14 +21,27 @@ const UserProfileCard = ({
   const taskClasses = cx(styles.profile_card, {
     [styles.activeCreateTask]: isSecureContext === true,
   });
+  const handlerErrorAvatar = ({ target }) => {
+    target.remove();
+  };
+
   return (
     <article className={taskClasses}>
       {selectedUser.avatar ? (
-        <img
-          src={`${IMAGE_URL}/${selectedUser.avatar}`}
-          alt={selectedUser.name}
-          className={styles.avatar}
-        />
+        <div className={styles.card_avatar_wrapper}>
+          <div
+            className={styles.default_image}
+            style={{ backgroundColor: `${stringToColour(selectedUser.name)}` }}
+          >
+            {selectedUser.name[0]}
+          </div>
+          <img
+            src={`${IMAGE_URL}/${selectedUser.avatar}`}
+            className={styles.avatar}
+            alt={selectedUser.id}
+            onError={handlerErrorAvatar}
+          />
+        </div>
       ) : (
         <img
           src={defaultImage}
